e1976ed55498a94b6ad1f120fbcdd354d57aabc7
Javier <javiafellay96@gmail.com>
2020-12-26 22:44:20 -0300

    seccion sobre mi

---
+++ sobremi.php
@@ -0,0 +1,48 @@
+<?php
+ include_once 'includes/templates/header.php';
+?>
+<?php include_once 'includes/templates/barra.php'; ?>
+<div class="contenedor">
+	<h1 class="sobre-mi">Un poco de mi Persona</h1>
+<div class="contenedor-hoja">
+	<div class="principal">
+		<h2 class="javi-name">Javier Martinez Guanez</h2>
+		<img class="imagen-principal" src="img/Captura.PNG" alt="">
+		<p class="parrafo-sobre">Web Developer</p>
+		<h3 class="contacto-sobre acerca">Acerca de mí:</h3>
+		<p class="parrafo-sobre princi"><cite>"El trabajo duro, tarde o temprano tiene frutos"</cite> ,apasionado por las nuevas tecnologias estoy a un paso de culminar la carrera de ingeniería informática.<br>Me considero una persona sociable, con capacidad de trabajar en equipo, con mucha responsabilidad y honestidad<br>También considero que mi sed de conocimiento es insaciable y siempre estoy abierto a aprender cosas nuevas</p>	
+
+		<h3 class="contacto-sobre">Contáctame</h3>
+		<p class="parrafo-contacto">(0972)458399</p>
+		<p class="parrafo-contacto">javiafellay96@gmail.com</p>
+
+		<h3 class="contacto-sobre">Habilidades Destacadas</h3>
+		<ul class="ul">
+			<li class="lista-habilidades">HTML5</li>
+			<li class="lista-habilidades">CSS</li>
+			<li class="lista-habilidades">Bootstrap</li>
+			<li class="lista-habilidades">JavaScript</li>
+			<li class="lista-habilidades">Jquery</li>
+			<li class="lista-habilidades">PHP</li>
+			<li class="lista-habilidades">LARAVEL</li>
+		</ul>
+	</div>
+
+	<div>
+		<h2 class="proyecto">Proyectos</h2>
+		<h3 class="contacto-sobre acerca">Sitio Reserva de Conferencias</h3>
+		<p class="parrafo-sobre proyect">En este Proyecto se utizo varias Tecnologias, en la cuál podemos mencionar a Html5,Css3, JavaScript, Jquery, Ajax, Php7, Mysql. Se utilizo plugins e implemento un método de pago con PayPal</p>
+
+		<h3 class="contacto-sobre acerca">Punto de Venta:</h3>
+		<p class="parrafo-sobre proyect">Este proyecto esta muy enfocado a la administración de negocios a la vez que utiliza muchas herramientas para su desarrollo.</p>
+
+		<h3 class="contacto-sobre acerca">Red Social de Recetas:</h3>
+		<p class="parrafo-sobre proyect">Este proyecto realizado en Laravel 8 y utilizando VueJs, esta enfocado en que los usuarios puedan agregar sus recetas y votar por sus favoritas</p>
+	</div>
+
+</div><!--.Contenedor Hoja-->
+			<p class="visitar">Para ver el código de los proyectos pueden vistar mi perfil de Github más abajo, pronto agregaré los links a cada una</p>
+</div>
+
+<?php include_once 'includes/templates/footer.php'; ?>
+<script src="js/main.js"></script>
